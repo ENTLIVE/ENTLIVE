a51061674a6ddde7c2ca7a6004bb795de4a17ea4
zzdxwangdong <zzdxwangdong@gmail.com>
2018-03-21 15:25:39 +0800

    添加腾讯云的IM和推拉流

--- app/src/main/java/com/newstyle/entlive/WelcomeActivity.java
+++ app/src/main/java/com/newstyle/entlive/WelcomeActivity.java
@@ -1,16 +1,22 @@
 package com.newstyle.entlive;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.WindowManager;
 
 import com.newstyle.entlive.base.BaseActivity;
+import com.newstyle.entlive.util.LogUtil;
+import com.tencent.imsdk.TIMManager;
+import com.tencent.rtmp.TXLiveBase;
 
 /**
  * Created by wangdong on 2018/3/13.
  */
 
 public class WelcomeActivity extends BaseActivity{
+
+    private static final String TAG = WelcomeActivity.class.getSimpleName();
 
 
     @Override
@@ -20,6 +26,10 @@ public class WelcomeActivity extends BaseActivity{
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         setContentView(R.layout.activity_welcome_layout);
+
+        String sdkver = TXLiveBase.getSDKVersionStr();
+        LogUtil.log(TAG,sdkver);
+
     }
 
 
